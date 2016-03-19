fcd548d1cccf29b288c5f776f394111e7c71c917
troutcolor <johnjohnston@mac.com>
2016-03-19 19:45:56 +0000

    loader image and tydy code

--- river5.php
+++ river5.php
@@ -11,18 +11,14 @@
 define( 'RIVER5_URL', \plugin_dir_url( __FILE__ ) );
 
 ///just_a_test.mp3
-function river5_shortcode_routine($args) {
-		extract( shortcode_atts( array(
-			'river5jsonurl' => 'http://pi.johnj.info/rivers/Edublogs.js',
-		), $args ) );
+function river5_shortcode_routine( $args ) {
+	extract( shortcode_atts( array( 'river5jsonurl' => 'http://pi.johnj.info/rivers/Edublogs.js'), $args ) );
 	$return = "";	
-	// paty attention 
+	// pay attention john
 	$return= sprintf(
-		"<div  data-river5='%s' class='river5feed'  >test</div>",
+		"<div  data-river5='%s' class='river5feed'  ><img src='".RIVER5_URL."assets/Ajax-loader.gif'></div>",
 		esc_url( $river5jsonurl ) 
 	);
-	
-	
 	//enqueue here so only add script & styles when needed
 	wp_enqueue_script( 'river5');	
 	wp_enqueue_style ( 'river5' );
@@ -30,11 +26,9 @@ function river5_shortcode_routine($args) {
 	return $return;
 }
 
-
 //add_shortcode('river5', 'river5_shortcode_routine');
 //Above comment out because:  however when running in the plugin context you must hook the shortcode registration to init.
 //see https://developer.wordpress.org/plugins/shortcodes/basic-shortcodes/
-//[gifmovie gif="value1" mp3="value2"]
 
 function river5_register_shortcode() {
     add_shortcode( 'river5', 'river5_shortcode_routine' );
@@ -45,8 +39,9 @@ add_action( 'init', 'river5_register_shortcode' );
  
 function add_scripts_basic(){
 // wp_register_script Registers a script file in WordPress to be linked to a page later using the wp_enqueue_script() function, which safely handles the script dependencies.
-		wp_register_script( 'river5', plugins_url( 'assets/river5.js', __FILE__ ), array( 'json2','jquery' ),false,true );	
-	    wp_register_style ( 'river5', plugins_url( 'assets/river5.css', __FILE__ ) );
+	wp_register_script( 'river5', plugins_url( 'assets/river5.js', __FILE__ ), array( 'json2','jquery' ),false,true );	
+	wp_register_style ( 'river5', plugins_url( 'assets/river5.css', __FILE__ ) );
 }
+
 add_action( 'init', 'add_scripts_basic' );
 ?>
